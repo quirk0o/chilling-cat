01101ab859355c88a127462c9c0f98e22c4dcff5
Quirk00 <beata@obrok.eu>
2014-10-29 23:07:21 +0100

    Connect to mongodb

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,6 +10,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
